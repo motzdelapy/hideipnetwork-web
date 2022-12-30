d2d068de563ac2d714ca6932aeface12bb11f869
niwiu <120763486+niwiu@users.noreply.github.com>
2022-12-30 17:20:39 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: `/server/`,
+    prefix: `/https://cf.niwiu.app//`,
     bare: `/bare/`,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
