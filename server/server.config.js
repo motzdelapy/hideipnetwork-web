d2977cdd2bf1fbb401101e7e9d04594721b74790
niwiu <120763486+niwiu@users.noreply.github.com>
2022-12-30 17:18:05 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: `/server/`,
     bare: `/bare/`,
     encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/server/server.handler.js',
     client: '/server/server.client.js',
     bundle: '/server/server.bundle.js',
