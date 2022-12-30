8703317c4ba5f0c965dc4d20a418a37c9ccebd29
niwiu <120763486+niwiu@users.noreply.github.com>
2022-12-30 16:45:48 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
     prefix: `/server/`,
     bare: `/bare/`,
-    encodeUrl:https://cf.iumiu.com/,
-    decodeUrl:https://cf.iumiu.com/,
+    encodeUrl: https://cf.iumiu.com/,
+    decodeUrl: https://cf.iumiu.com/,
     handler: '/server/server.handler.js',
     client: '/server/server.client.js',
     bundle: '/server/server.bundle.js',
