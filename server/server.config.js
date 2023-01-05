d20fd3b52f4b86c33a06de3ef8b763f8db7ff8a7
niwiu <120763486+niwiu@users.noreply.github.com>
2023-01-05 11:41:53 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://yourdomain'
+const BAREURL = 'https://autumn-mouse-2523.iumiu.workers.dev'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
