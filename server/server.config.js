8f09a3c097a796875f68c3944bf6a9c31f0778a5
niwiu <120763486+niwiu@users.noreply.github.com>
2023-01-05 12:23:52 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://cf.niwiu.app/'
+const BAREURL = 'https://cdn.niwiu.com/'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
